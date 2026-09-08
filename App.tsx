12b75d534bcfb75eea766559445e3158bb1c4be1
Melusi <st10525308@myemeris.edu.za>
2026-09-08 13:25:22 +0200

    info passing to next screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
-// App.tsx
 import { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
+// NavigationContainer wraps the whole app and manages navigation state
+import { NavigationContainer } from '@react-navigation/native';
+// createNativeStackNavigator builds a stack-based navigator using native transitions
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  const [player1, setPlayer1] = useState<string>('');
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={PlayerScreen} />
+        <Stack.Screen name="Game" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function PlayerScreen({ navigation }: any){
+    const [player1, setPlayer1] = useState<string>('');
   const [player2, setPlayer2] = useState<string>('');
 
   return (
@@ -25,8 +42,17 @@ export default function App() {
 
       <Button
         title="Start Game"
-        onPress={() => console.log(`Player 1: ${player1}, Player 2: ${player2}`)}
+        onPress={() => navigation.navigate('Game', {player1: player1, player2: player2})}
       />
+    </View>
+  );
+}
+
+function GameScreen({ navigation, route} : any){
+  const { player1, player2 } = route.params; 
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>{player1} vs. {player2}</Text>
     </View>
   );
 }
